2d9302517dee0c8428574f3bddb0a03854ecfd40
marianraven <mariangsdh@gmail.com>
2024-09-09 14:17:49 -0300

    :art: Cambiando lexer  a variables en español por pedido del profe

--- mqtt_lexer.py
+++ mqtt_lexer.py
@@ -2,41 +2,41 @@ import ply.lex as lex
 
 # Definición de tokens iniciales
 tokens = (
-    'DEVICE', 'MESSAGE', 'SEND', 'TO', 'WITH', 'KEY', 'VALUE',
-    'ENCRYPT', 'AUTH', 'CONCURRENT', 'QUEUE', 'MQTT', 'TOPIC',
-    'CONNECT', 'DISCONNECT', 'PUBLISH', 'SUBSCRIBE', 'UNSUBSCRIBE'
+    'DISPOSITIVO', 'MENSAJE', 'ENVIAR', 'A', 'CON', 'KEY', 'VALOR',
+    'ENCRIPTADO', 'AUTH', 'CONCURRENTE', 'COLA', 'MQTT', 'TOPIC',
+    'CONECTAR', 'DESCONECTAR', 'PUBLICAR', 'SUSCRIBIRSE', 'DESUSCRIBIRSE'
 )
 
 # Expresiones regulares para los tokens
-t_SEND = r'send'
-t_TO = r'to'
-t_WITH = r'with'
+t_ENVIAR = r'enviar'
+t_A = r'a'
+t_CON = r'con'
 t_KEY = r'key'
-t_VALUE = r'\"[^\"]*\"'
-t_ENCRYPT = r'encrypt'
+t_VALOR = r'\"[^\"]*\"'
+t_ENCRIPTADO = r'encriptado'
 t_AUTH = r'auth'
-t_CONCURRENT = r'concurrent'
-t_QUEUE = r'queue'
+t_CONCURRENTE = r'concurrente'
+t_COLA = r'cola'
 t_MQTT = r'mqtt'
 t_TOPIC = r'topic'
-t_CONNECT = r'connect'
-t_DISCONNECT = r'disconnect'
-t_PUBLISH = r'publish'
-t_SUBSCRIBE = r'subscribe'
-t_UNSUBSCRIBE = r'unsubscribe'
-t_ignore = ' \t'
+t_CONECTAR = r'conectar'
+t_DESCONECTAR = r'desconectar'
+t_PUBLICAR = r'publicar'
+t_SUSCRIBIRSE = r'suscribirse'
+t_DESUSCRIBIRSE = r'desuscribirse'
+t_ignorar = ' \t'
 
 def t_DEVICE(t):
-    r'device_[0-9]+'
+    r'dispositivo_[0-9]+'
     return t
 
-def t_MESSAGE(t):
-    r'message_[a-zA-Z_]+'
+def t_MENSAJE(t):
+    r'mensaje_[a-zA-Z_]+'
     return t
 
 # Manejo errores léxicos
 def t_error(t):
-    print(f"Illegal character '{t.value[0]}'")
+    print(f"Caracter ilegal '{t.valor[0]}'")
     t.lexer.skip(1)
 
 # Construcción del lexer
